ba5bef20dfcc4df29adec6866f027b0af23262a3
andreinwald <andreinwald@gmail.com>
2024-04-21 10:11:05 +0200

    version

--- dist/Resolve.d.ts
+++ dist/Resolve.d.ts
@@ -1,3 +1,5 @@
 import { URIComponents } from "./index";
-export declare function resolve(baseURI: string, relativeURI: string): string;
-export declare function resolveComponents(base: URIComponents, relative: URIComponents): URIComponents;
+export declare function resolve(baseURI: string, relativeURI: string, options?: any): string;
+export declare function resolveComponents(base: URIComponents, relative: URIComponents, options: {
+    tolerant?: any;
+}, skipNormalization: boolean): URIComponents;
